69fdd377004bf5bf814b80565057b5a3bc6fab1a
Vadim <vtimofeev223@gmail.com>
2021-03-22 12:22:56 +0300

    init

---
+++ app/controllers/MainController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Controllers;
+
+use Core\Controller;
+use Models\User;
+
+class MainController extends Controller
+{
+    public function index()
+    {
+        $this->view('main/index', []);
+    }
+}
